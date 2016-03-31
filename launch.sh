55ed726d9214659ed13cdcb0c374c2a0a1d49205
Mustafa ip <mustafa_hyp@yahoo.com.tw>
2016-03-31 13:10:36 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -33,15 +33,16 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
+  ./.luarocks/bin/luarocks install luasec
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install oauth
+  ./.luarocks/bin/luarocks install lbase64 20120807-3
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
+
 
   ./.luarocks/bin/luarocks install redis-lua
   RET=$?; if [ $RET -ne 0 ];
@@ -110,7 +111,8 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  #logo create by Mustafa ip (HackeD_o)
+  
+#logo create by Mustafa ip (HackeD_o)
    echo -e "\033[38;5;208m"
    echo -e "      ▀▄   ▄▀       "
    echo -e "     ▄█▀███▀█▄      "
