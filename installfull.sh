94d9d3f588d1aea7d69365e6455d7619f8930ad0
Mustafa ip <mustafa_hyp@yahoo.com.tw>
2016-03-31 14:07:48 -0700

    Update installfull.sh

--- installfull.sh
+++ installfull.sh
@@ -1,5 +1,1 @@
-git clone https://github.com/mustafa1p/SKSUPER.git && cd SKSUPER && chmod +x launch.sh && ./launch.sh install && cd SKSUPER && ./launch.sh install
-sudo ./.luarocks/bin/luarocks install luafilesystem
-sudo ./.luarocks/bin/luarocks install lub
-sudo ./.luarocks/bin/luarocks install luaexpat
-./launch.sh install
+git clone https://github.com/mustafa1p/SKSUPER.git && cd SKSUPER && chmod +x launch.sh && ./launch.sh install && cd SKSUPER && ./launch.sh install && sudo ./.luarocks/bin/luarocks install luafilesystem && sudo ./.luarocks/bin/luarocks install lub && sudo ./.luarocks/bin/luarocks install luaexpat && ./launch.sh install && ./launch.sh
